350b25695eeed7841097480b1e7e2299a6679e3c
josevitorsg <josevitor958@gmail.com>
2025-09-22 09:55:22 -0300

    pequena atualização para subir os arquivos no netlify

---
+++ src/service-worker.js
@@ -0,0 +1,23 @@
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open("app-saudacao-cache").then((cache) => {
+      return cache.addAll([
+        "index.html",
+        "manifest.json",
+        "/image/icon-192.png",
+        "/image/icon-512.png",
+        "/image/morning.jpg",
+        "/image/afternoon.jpg",
+        "/image/night.jpg",
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    cashes
+      .match(event.request)
+      .then((response) => response || fetch(event.request))
+  );
+});
